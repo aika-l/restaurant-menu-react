dc0571e586714c949638256d63c136f91094df18
aika-l <67886657+aika-l@users.noreply.github.com>
2026-09-02 14:50:13 -0500

    Add menu data for food items
    This file contains a menu array with various food items, including their titles, categories, prices, images, and descriptions.

---
+++ src/components/Data.jsx
@@ -0,0 +1,84 @@
+const menu = [
+  {
+    id: 1,
+    title: "buttermilk pancakes",
+    category: "breakfast",
+    price: 15.99,
+    img:
+      "https://www.pngkit.com/png/detail/16-165867_buttermilk-pancakes-whipped-topping-and-sweet-ihop-blueberry.png",
+    desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `
+  },
+  {
+    id: 2,
+    title: "diner double",
+    category: "lunch",
+    price: 13.99,
+    img:
+      "https://static01.nyt.com/images/2021/02/17/dining/17tootired-grilled-cheese/17tootired-grilled-cheese-articleLarge.jpg?quality=75&auto=webp&disable=upscale",
+    desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `
+  },
+  {
+    id: 3,
+    title: "godzilla milkshake",
+    category: "shakes",
+    price: 6.99,
+    img:
+      "https://www.laxshopdine.com/wp-content/uploads/2019/01/SSP_Cassells_Milkshakes.jpg",
+    desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`
+  },
+  {
+    id: 4,
+    title: "country delight",
+    category: "breakfast",
+    price: 20.99,
+    img: "https://www.elpolloloco.com/content/img/our-food_375.jpg",
+    desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `
+  },
+  {
+    id: 5,
+    title: "egg attack",
+    category: "lunch",
+    price: 22.99,
+    img:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbtPawTJL4xvL7KZzue61-725_KSGZJwch3w&usqp=CAU",
+    desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `
+  },
+  {
+    id: 6,
+    title: "oreo dream",
+    category: "shakes",
+    price: 18.99,
+    desc:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi, unde fugit temporibus commodi dolore praesentium velit id natus quia te illum delectus!",
+    img:
+      "https://www.wienerschnitzel.com/wp-content/uploads/2017/06/Oreo-Shake-493x296-1.jpg"
+  },
+  {
+    id: 7,
+    title: "bacon overflow",
+    category: "breakfast",
+    price: 8.99,
+    img: "https://www.elpolloloco.com/content/img/our-food_375.jpg",
+    desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `
+  },
+  {
+    id: 8,
+    title: "american classic",
+    category: "lunch",
+    price: 12.99,
+    img:
+      "https://static01.nyt.com/images/2021/02/17/dining/17tootired-grilled-cheese/17tootired-grilled-cheese-articleLarge.jpg?quality=75&auto=webp&disable=upscale",
+    desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `
+  },
+  {
+    id: 9,
+    title: "quarantine buddy",
+    category: "shakes",
+    price: 16.99,
+    img:
+      "https://health.clevelandclinic.org/wp-content/uploads/sites/3/2016/10/foodJointPainRelief-142336977-770x533-1.jpg",
+    desc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur nihil sed eos atque quo quisquam."
+  }
+];
+export default menu;
